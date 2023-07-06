96fa62e94cb780bce06db31a8ff9e81cfe6ed138
Javad Ghanbari <javadghanbari.put@gmail.com>
2023-07-06 16:12:41 -0700

    Adding styles

--- crypto-react/src/components/Coin.js
+++ crypto-react/src/components/Coin.js
@@ -1,13 +1,15 @@
 import React from 'react';
+//Style
+import styles from "./Coin.module.css";
 
 const Coin = ({name,image,symbol,price,priceChange,marketCap}) => {
     return (
-        <div>
-            <img src={image} alt={name} />
-            <span>{name}</span>
-            <span>{symbol.toUppercase()}</span>
-            <span>{price.toLocalString()}</span>
-            <span>{priceChange.toFixed(2)}</span>
+        <div className={styles.container}>
+            <img className={styles.image} src={image} alt={name} />
+            <span className={styles.name}>{name}</span>
+            <span className={styles.symbol}>{symbol.toUppercase()}</span>
+            <span className={styles.currenPrice}>{price.toLocalString()}</span>
+            <span className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}>{priceChange.toFixed(2)}</span>
             <span>{marketCap.toLocalString()}</span>
         </div>
     );
--- crypto-react/src/components/Landing.js
+++ crypto-react/src/components/Landing.js
@@ -7,6 +7,9 @@ import { getCoin } from '../services/api';
 import Loader from './Loader';
 import Coin from './Coin';
 
+//Styles
+import styles from "./Landing.module.css"; 
+
 const Landing = () => {
 
     const [coins, setCoins] = useState([]);
@@ -19,30 +22,34 @@ const Landing = () => {
         }
 
         fetchAPI()
-    }, [])
+    },[])
 
     const searchHandler = event => {
         setSearch(event.target.value)
     }
-    const searchCoins = coins.filter(coin => coin.name.toLowercase().include(search.toLowercase()))
+    const searchedCoins = coins.filter(coin => coin.name.toLowercase().include(search.toLowercase()))
 
     return (
         <>
-            <input type="text" placeholder="Search" value={search} onChange={searchHandler} />
-            <div>
-                {coins.length ? <div>{
-                    searchCoins.map(coin => <Coin key={coin.id}
-                        name = {coin.name}
-                        image = {coin.image}
-                        symbole = {coin.symbole}
-                        price = {coin.current_price}
-                        priceChange = {coin.price_change_percentage_24h}
-                        marketCap = {coin.market_cap}
-                        
-                        />)
-                }</div> : <Loader />}
-                
-             </div> 
+            <input className={styles.input} type="text" placeholder="Search" value={search} onChange={searchHandler} />
+            {
+                coins.length ?
+                    <div className={styles.coinContainer}>
+                        {
+                            searchedCoins.map(coin => <Coin
+                                key={coin.id}
+                                name={coin.name}
+                                image={coin.image}
+                                symbol={coin.symbol}
+                                price={coin.current_price}
+                                marketCap={coin.market_cap}
+                                priceChange={coin.price_change_percentage_24h}
+                            />)
+                        }
+                    </div> :
+
+                    <Loader />
+            }
         </>
     );
 };
