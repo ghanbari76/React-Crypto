6b748303fc8837f9d9c76366160864f358509f93
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-04-10 16:05:54 -0700

    Update BASE_URL

--- crypto-react/src/services/api.js
+++ crypto-react/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&locale=en';
+const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&x-cg-pro-api-key: CG-GYCPmpxsoB28JraN4q4mVfvF';
+
 const getCoin = async () => {
     const response = await axios.get(BASE_URL);
     return response.data;
