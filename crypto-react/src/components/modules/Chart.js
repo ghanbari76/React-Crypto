9fad82b6932e4950a2924e13104050e127617ac9
Javad Ghanbari <javadghanbari.put@gmail.com>
2024-05-22 19:43:27 -0700

    Drawing a diagram

--- crypto-react/src/components/modules/Chart.js
+++ crypto-react/src/components/modules/Chart.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, YAxis } from "recharts";
 
 import { convertData } from '../../helpers/convertData';
 //Style
@@ -12,9 +13,27 @@ const Chart = ({ chart, setChart }) => {
             <span className={styles.cross} onClick={() => setChart(null)}>
                 X
             </span>
-            <div className={styles.chart}></div>
+            <div className={styles.chart}>
+                <div className={styles.graph}>
+                    <ChartComponent data={convertData(chart, type)} type={type} />
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Chart;
+
+const ChartComponent = ({ data, type }) => {
+    return (
+        <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={400} height={400} data={data} >
+                <CartesianGrid stroke='#404042' />
+                <Line type="monotone" dataKey={type} stroke="#3874ff" strokeWidth="2px" />
+                <YAxis dataKey={type} domain={["auto", "auto"]} />
+                <Legend />
+                <Tooltip />
+            </LineChart>
+        </ResponsiveContainer>
+    );
+} 
